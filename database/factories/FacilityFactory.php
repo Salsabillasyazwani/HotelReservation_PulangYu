73db62736df4cffd5e020a77f3bdf0254499b095
Salsabillasyazwani <syazwanibila@gmail.com>
2026-07-09 21:36:34 +0700

    Clean up comments in FacilityFactory.php
    Removed unnecessary comments from FacilityFactory.php.

--- database/factories/FacilityFactory.php
+++ database/factories/FacilityFactory.php
@@ -4,19 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<\App\Models\Facility>
- */
 class FacilityFactory extends Factory
 {
     protected $model = \App\Models\Facility::class;
-
-    /**
-     * Default definition (dipakai kalau Facility::factory() dipanggil tanpa
-     * override). FacilitySeeder akan meng-override 'name', 'icon', dan
-     * 'description' dengan data realistis lewat state(), jadi definition
-     * default di sini cukup sebagai fallback generik.
-     */
     public function definition(): array
     {
         return [
